5661d60b59c8a14097cc22c0997c54b42729a3df
rgunthertap <136386221+rgunthertap@users.noreply.github.com>
2023-06-21 03:12:09 +0000

    Start to learn C#

---
+++ HelloWorld.cs
@@ -0,0 +1,10 @@
+class HelloWorld
+{
+    public static void Main()
+    {
+        System.Console.WriteLine("Hello, world!"); // prints some text on to the screen
+        System.Console.ReadKey(); /* waits for the user
+        to press a key
+        */
+    }
+}
