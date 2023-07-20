2b9f0add10fbba4f312d71f7f819389016038fc9
rgunthertap <136386221+rgunthertap@users.noreply.github.com>
2023-07-20 18:24:06 +0000

    Create Bank app

--- Hello/Program.cs
+++ Hello/Program.cs
@@ -1,2 +1,14 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+//Console.WriteLine("Hello, World!!!!!");
+class HelloWorld
+{
+    public static void Main(string[] args)
+    {
+        var names = new List<string>{"<name>", "Ana", "Jack", "Kate"};
+        foreach (var name in names)
+        {
+            Console.WriteLine($"Hello {name}");
+        }
+
+    }
+}
