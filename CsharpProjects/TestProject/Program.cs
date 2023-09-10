ce670b6eb23eed9131ebb916030ab126ef7a10b0
rgunthertap <136386221+rgunthertap@users.noreply.github.com>
2023-09-10 01:55:47 +0000

    remove junk

---
+++ CsharpProjects/TestProject/Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+Random random = new Random();
+int current = 0;
+
+do
+{
+    current = random.Next(1, 11);
+    Console.WriteLine(current);
+} while (current != 7);
+
---
+++ SayHello/SayHelloUI/Program.cs
@@ -0,0 +1,15 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Console.WriteLine(2.625f);
+//Console.WriteLine(12.3981m);
+string name = "Bob";
+int three = 3;
+float temp = 34.4f;
+
+Console.Write("Hello, ");
+Console.Write(name);
+Console.Write("! You have ");
+Console.Write(three);
+Console.Write(" in your inbox. The temperature is ");   
+Console.Write(temp);
+Console.Write(" celsius.");    
+
